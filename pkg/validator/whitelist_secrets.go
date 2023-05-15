e53e9a8c9f778886e2fa94693ce26d9addd286e7
yushink <ysd.kang@gmail.com>
2023-05-15 15:48:11 +0900

    refactor comment

--- pkg/validator/whitelist_secrets.go
+++ pkg/validator/whitelist_secrets.go
@@ -3,8 +3,9 @@ package validator
 import "github.com/nullify-platform/config-file-parser/pkg/models"
 
 func ValidateSecrets(config *models.Configuration) bool {
-	// the list of strings can be any mix of characters so no real validation needed yet at this step
-	// the yaml marshaller will check if it's not a list of strings
+	// the list of strings can be any mix of characters so no validation needed at this stage
+	// this will change as we start accepint files
+	// the parser will check if it's a valid list of strings
 	if config.SecretsWhitelist == nil {
 		return true
 	}
