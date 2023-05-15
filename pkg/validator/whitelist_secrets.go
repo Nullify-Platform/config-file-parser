806de3b07a4c80ac09014d8c6a4e817bf6f102ba
yushindkang <87850091+yushindkang@users.noreply.github.com>
2023-05-15 16:34:20 +0900

    Add secrets whitelist (#24)
    * add secretswhilist var
    
    * add test and comment
    
    * refactor comment
    
    * Update pkg/validator/whitelist_secrets.go
    
    Co-authored-by: dstrates <99311378+dstrates@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: dstrates <99311378+dstrates@users.noreply.github.com>

---
+++ pkg/validator/whitelist_secrets.go
@@ -0,0 +1,14 @@
+package validator
+
+import "github.com/nullify-platform/config-file-parser/pkg/models"
+
+func ValidateSecrets(config *models.Configuration) bool {
+	// the list of strings can be any mix of characters so no validation needed at this stage
+	// this will change as we start accepting files
+	// the parser will check if it's a valid list of strings
+	if config.SecretsWhitelist == nil {
+		return true
+	}
+
+	return true
+}
