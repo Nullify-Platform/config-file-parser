95004931c9ea02da88f6512cc0f0a9edfb4dce45
Joshua Fish <joshua.fish@nullify.com>
2023-06-16 12:45:03 +1000

    added ignore patterns and added tests for glob patterns

--- pkg/models/config.go
+++ pkg/models/config.go
@@ -3,6 +3,7 @@ package models
 type Configuration struct {
 	SeverityThreshold  string   `yaml:"severity_threshold"`
 	IgnoreDirs         []string `yaml:"ignore_dirs"`
+	IgnorePatterns     []string `yaml:"ignore_patterns"`
 	EmailNotifications []string `yaml:"email_notifications"`
 	SecretsWhitelist   []string `yaml:"secrets_whitelist"`
 }
