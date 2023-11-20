f0378c04284061fe3c42132292aa8303cb5297a9
RahulTripathi0401 <51304540+RahulTripathi0401@users.noreply.github.com>
2023-11-20 13:07:04 -0800

    adding fail build to config (#68)
    * adding fail build to config
    
    * changing names from build to builds
    
    * changing build to builds
    
    ---------
    
    Co-authored-by: Rahul Tripathi <rahul.tripathi@nullify.ai>

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -1,6 +1,7 @@
 package models
 
 type Configuration struct {
+	FailBuilds             bool                             `yaml:"fail_builds,omitempty"`
 	SeverityThreshold      string                           `yaml:"severity_threshold,omitempty"`
 	IgnoreDirs             []string                         `yaml:"ignore_dirs,omitempty"`
 	IgnorePaths            []string                         `yaml:"ignore_paths,omitempty"`
--- pkg/parser/defaults.go
+++ pkg/parser/defaults.go
@@ -6,6 +6,7 @@ const DefaultSeverityThreshold = models.SeverityMedium
 
 func NewDefaultConfig() *models.Configuration {
 	return &models.Configuration{
+		FailBuilds:             false,
 		SeverityThreshold:      DefaultSeverityThreshold,
 		IgnoreDirs:             []string{},
 		IgnorePaths:            []string{},
