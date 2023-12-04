7ece585a216f94620a0be2b88fc3e609fa5f7dc6
RahulTripathi0401 <51304540+RahulTripathi0401@users.noreply.github.com>
2023-12-04 17:04:13 +1100

    converint bool to *bool (#70)
    Co-authored-by: Rahul Tripathi <rahul.tripathi@nullify.ai>

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -10,8 +10,9 @@ import (
 )
 
 func TestIntegration(t *testing.T) {
+	fail := true
 	expectedConfig := &models.Configuration{
-		FailBuilds:        true,
+		FailBuilds:        &fail,
 		SeverityThreshold: models.SeverityMedium,
 		IgnoreDirs:        []string{"dir1"},
 		IgnorePaths:       []string{"data/**/*"},
@@ -136,6 +137,139 @@ func TestIntegration(t *testing.T) {
 	}
 
 	config, err := parser.LoadFromFile("nullify.yaml")
+	require.NoError(t, err)
+
+	require.Equal(t, expectedConfig, config)
+	require.True(t, validator.ValidateConfig(config))
+}
+
+func TestEmptyFailsBuildField(t *testing.T) {
+	expectedConfig := &models.Configuration{
+		FailBuilds:        nil,
+		SeverityThreshold: models.SeverityMedium,
+		IgnoreDirs:        []string{"dir1"},
+		IgnorePaths:       []string{"data/**/*"},
+		SecretsWhitelist:  []string{"abcd1234"},
+		Secrets: models.Secrets{
+			Ignore: []models.SecretsIgnore{
+				{
+					Value:  "mocksecret123",
+					Reason: "This is a test secret, it has no access to anything",
+					Paths:  []string{"**/tests/*"},
+				},
+				{
+					Pattern: "id[0-9]+",
+					Reason:  "These are not secrets, they are internal identifiers",
+				},
+				{
+					Value:  "actualsecret123",
+					Reason: "We can't remove this right now but we should",
+					Expiry: "2021-12-31",
+				},
+			},
+		},
+		Notifications: map[string]models.Notification{
+			"all-events-webhook": {
+				Events: models.NotificationEvents{
+					All: &models.NotificationEventAll{
+						MinimumSeverity: models.SeverityHigh,
+						SecretTypes:     []string{"ssh_key"},
+					},
+				},
+				Targets: models.NotificationTargets{
+					Webhook: &models.NotificationTargetWebhook{
+						URLs: []string{"https://webhook.site/123456"},
+					},
+				},
+			},
+			"findings-to-slack-and-email": {
+				Events: models.NotificationEvents{
+					NewCodeFindings: &models.NotificationEventNewCodeFindings{
+						MinimumSeverity: models.SeverityHigh,
+					},
+					NewSecretFindings: &models.NotificationEventNewSecretFindings{
+						Types: []string{"ssh_key"},
+					},
+					NewDependencyFindings: &models.NotificationEventNewDependencyFindings{
+						MinimumSeverity: models.SeverityHigh,
+					},
+				},
+				Targets: models.NotificationTargets{
+					Slack: &models.NotificationTargetSlack{
+						Channels: []string{"123456"},
+					},
+					Email: &models.NotificationTargetEmail{
+						Addresses: []string{"notifications@nullify.ai", "noreply@nullify.ai"},
+					},
+				},
+				Repositories: []string{
+					"config-file-parser",
+					"dast-action",
+					"cli",
+				},
+			},
+		},
+		ScheduledNotifications: map[string]models.ScheduledNotification{
+			"new-findings": {
+				Schedule: "0 0 * * *",
+				Topics: models.ScheduledNotificationTopics{
+					All: true,
+				},
+				Targets: models.ScheduledNotificationTargets{
+					Email: &models.ScheduledNotificationTargetEmail{
+						Addresses: []string{"notifications@nullify.ai", "noreply@nullify.ai"},
+					},
+					Slack: &models.ScheduledNotificationTargetSlack{
+						Channels: []string{"123456"},
+					},
+				},
+				Repositories: []string{
+					"config-file-parser",
+					"dast-action",
+					"cli",
+				},
+			},
+		},
+		Code: models.Code{
+			Ignore: []models.CodeIgnore{
+				{
+					CWEs:   []int{589},
+					Reason: "HTTP requests with variables in tests don't matter",
+					Paths:  []string{"**/tests/*"},
+					Repositories: []string{
+						"config-file-parser",
+						"dast-action",
+						"cli",
+					},
+				},
+				{
+					RuleIDs: []string{"python-sql-injection"},
+					Reason:  "This code won't be going live until next year but we should fix it before then",
+					Expiry:  "2021-12-31",
+				},
+			},
+		},
+		Dependencies: models.Dependencies{
+			Ignore: []models.DependenciesIgnore{
+				{
+					CVE:    "CVE-2021-1234",
+					Reason: "This is a false positive",
+					Expiry: "2021-12-31",
+				},
+				{
+					CVE:    "CVE-2021-5678",
+					Reason: "This isn't exploitable in client applications",
+					Expiry: "2021-12-31",
+					Repositories: []string{
+						"dast-action",
+						"cli",
+					},
+				},
+			},
+		},
+	}
+
+	config, err := parser.LoadFromFile("empty_fail_build.yaml")
 	require.NoError(t, err)
 
 	require.Equal(t, expectedConfig, config)
