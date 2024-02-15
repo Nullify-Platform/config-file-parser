413cc9113265a324bf400a4aa6ae3db9fbcd3198
yushink <ysd.kang@gmail.com>
2024-02-15 16:56:58 +1100

    put default back in

--- pkg/parser/defaults.go
+++ pkg/parser/defaults.go
@@ -2,6 +2,8 @@ package parser
 
 import "github.com/nullify-platform/config-file-parser/pkg/models"
 
+const DefaultSeverityThreshold = models.SeverityMedium
+
 func NewDefaultConfig() *models.Configuration {
 	return &models.Configuration{
 		SeverityThreshold:      "",
