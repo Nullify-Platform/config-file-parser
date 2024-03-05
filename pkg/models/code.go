53b6815d7582072f9ce50b537280d0194fc1ca70
Tim Thacker <tim.thacker@nullify.cloud>
2024-03-05 23:59:28 +1100

    remove secrets whitelist, make cve filters a list and organise the structs better

--- pkg/models/code.go
+++ pkg/models/code.go
@@ -5,12 +5,14 @@ type Code struct {
 }
 
 type CodeIgnore struct {
+	Reason string `yaml:"reason,omitempty"`
+	Expiry string `yaml:"expiry,omitempty"`
+
+	// matchers
 	CWEs    []int    `yaml:"cwes,omitempty"`
 	RuleIDs []string `yaml:"rule_ids,omitempty"`
-	Reason  string   `yaml:"reason,omitempty"`
 	Dirs    []string `yaml:"dirs,omitempty"`
 	Paths   []string `yaml:"paths,omitempty"`
-	Expiry  string   `yaml:"expiry,omitempty"`
 
 	// global config only
 	Repositories []string `yaml:"repositories,omitempty"`
--- pkg/models/secrets.go
+++ pkg/models/secrets.go
@@ -5,12 +5,12 @@ type Secrets struct {
 }
 
 type SecretsIgnore struct {
-	Value   string   `yaml:"value,omitempty"`
-	Pattern string   `yaml:"pattern,omitempty"`
-	Reason  string   `yaml:"reason,omitempty"`
-	Expiry  string   `yaml:"expiry,omitempty"`
-	Dirs    []string `yaml:"dirs,omitempty"`
-	Paths   []string `yaml:"paths,omitempty"`
+	Reason string `yaml:"reason,omitempty"`
+	Expiry string `yaml:"expiry,omitempty"`
+
+	// matchers
+	Value   string `yaml:"value,omitempty"`
+	Pattern string `yaml:"pattern,omitempty"`
 
 	// global config only
 	Repositories []string `yaml:"repositories,omitempty"`
