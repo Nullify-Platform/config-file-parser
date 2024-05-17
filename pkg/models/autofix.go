b5c59d6477759fd1671850f4f06430d86c174060
Nicholas Albion <nicholas.albion@nullify.cloud>
2024-05-17 17:11:38 +1000

    NUL-98 add autofix `scripts` to for lint, compile, test

--- pkg/models/autofix.go
+++ pkg/models/autofix.go
@@ -4,9 +4,18 @@ type AutoFix struct {
 	Enabled                    bool                            `yaml:"enabled,omitempty"`
 	MaxPullRequestsOpen        *int                            `yaml:"max_pull_requests_open,omitempty"`
 	MaxPullRequestCreationRate *AutoFixPullRequestCreationRate `yaml:"max_pull_request_creation_rate,omitempty"`
+	Scripts                    *AutoFixScripts                 `yaml:"scripts,omitempty"`
 }
 
 type AutoFixPullRequestCreationRate struct {
 	Count int `yaml:"count,omitempty"`
 	Days  int `yaml:"days,omitempty"`
+}
+
+type AutoFixScripts struct {
+	// docker image - run locally if empty
+	Image   string `yaml:"image,omitempty"`
+	Lint    string `yaml:"lint,omitempty"`
+	Compile string `yaml:"compile,omitempty"`
+	Test    string `yaml:"test,omitempty"`
 }
