44497045e97d262144175b4942d8928e446f8c48
Kevin Zhu <kevin.yd.zhu@gmail.com>
2024-07-10 03:52:36 -0700

    feat: add priority ratings (#122)

---
+++ pkg/models/priority.go
@@ -0,0 +1,9 @@
+package models
+
+const (
+	PriorityUrgent     string = "URGENT"
+	PriorityImportant  string = "IMPORTANT"
+	PriorityMedium     string = "MEDIUM"
+	PriorityLow        string = "LOW"
+	PriorityNegligible string = "NEGLIGIBLE"
+)
