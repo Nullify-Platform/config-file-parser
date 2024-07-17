e1502403a8c6690c3fc4ca3db81395a935dcfb63
Tim Thacker <123459940+tim-thacker-nullify@users.noreply.github.com>
2024-07-17 10:40:41 +1000

    add extra auto-fix options (#125)

--- pkg/models/autofix.go
+++ pkg/models/autofix.go
@@ -4,6 +4,7 @@ type AutoFix struct {
 	Enabled                    bool                            `yaml:"enabled,omitempty"`
 	MaxPullRequestsOpen        *int                            `yaml:"max_pull_requests_open,omitempty"`
 	MaxPullRequestCreationRate *AutoFixPullRequestCreationRate `yaml:"max_pull_request_creation_rate,omitempty"`
+	Labels                     []string                        `yaml:"labels,omitempty"`
 }
 
 type AutoFixPullRequestCreationRate struct {
--- pkg/models/models.go
+++ pkg/models/models.go
@@ -5,14 +5,16 @@ type Configuration struct {
 	SeverityThreshold string   `yaml:"severity_threshold,omitempty"`
 	IgnoreDirs        []string `yaml:"ignore_dirs,omitempty"`
 	IgnorePaths       []string `yaml:"ignore_paths,omitempty"`
-
-	Code         Code         `yaml:"code,omitempty"`
-	Dependencies Dependencies `yaml:"dependencies,omitempty"`
-	Secrets      Secrets      `yaml:"secrets,omitempty"`
+	AutoFix           *AutoFix `yaml:"auto_fix,omitempty"`
 
 	Notifications          map[string]Notification          `yaml:"notifications,omitempty"`
 	ScheduledNotifications map[string]ScheduledNotification `yaml:"scheduled_notifications,omitempty"`
 	Integrations           Integrations                     `yaml:"integrations,omitempty"`
+
+	// features
+	Code         Code         `yaml:"code,omitempty"`
+	Dependencies Dependencies `yaml:"dependencies,omitempty"`
+	Secrets      Secrets      `yaml:"secrets,omitempty"`
 
 	// TODO deprecate
 	SecretsWhitelist []string `yaml:"secrets_whitelist,omitempty"`
