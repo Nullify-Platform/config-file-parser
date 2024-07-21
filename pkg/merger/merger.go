2b85d501a328bc143386e70b09236a0b4e5a56ad
Tim Thacker <123459940+tim-thacker-nullify@users.noreply.github.com>
2024-07-21 18:25:55 +1000

    merge list of config files (#127)

--- pkg/merger/merger.go
+++ pkg/merger/merger.go
@@ -2,152 +2,110 @@ package merger
 
 import (
 	"github.com/nullify-platform/config-file-parser/pkg/models"
-	"github.com/nullify-platform/config-file-parser/pkg/parser"
 	"github.com/nullify-platform/config-file-parser/pkg/validator"
 )
 
 func MergeConfigFiles(
-	globalConfig *models.Configuration,
-	repoConfig *models.Configuration,
+	defaultConfg *models.Configuration,
+	extraConfigs ...*models.Configuration,
 ) *models.Configuration {
-	config := parser.NewDefaultConfig()
+	if defaultConfg == nil {
+		return nil
+	}
 
-	// override with global config
+	// copy default config
+	config := *defaultConfg
 
-	if globalConfig != nil {
-		if globalConfig.FailBuilds != nil {
-			config.FailBuilds = globalConfig.FailBuilds
+	// loop over extraConfigs and override settings
+	for _, extraConfig := range extraConfigs {
+		if extraConfig == nil {
+			continue
 		}
 
-		if globalConfig.SeverityThreshold != "" && validator.ValidateSeverityThreshold(config) {
-			config.SeverityThreshold = globalConfig.SeverityThreshold
+		if extraConfig.FailBuilds != nil {
+			config.FailBuilds = extraConfig.FailBuilds
 		}
 
-		config.IgnoreDirs = globalConfig.IgnoreDirs
-		config.IgnorePaths = globalConfig.IgnorePaths
-
-		config.Code.AutoFix = globalConfig.Code.AutoFix
-		config.Code.Ignore = globalConfig.Code.Ignore
-		config.Dependencies.AutoFix = globalConfig.Dependencies.AutoFix
-		config.Dependencies.Ignore = globalConfig.Dependencies.Ignore
-		config.Secrets.Ignore = globalConfig.Secrets.Ignore
-		config.Secrets.CustomPatterns = globalConfig.Secrets.CustomPatterns
-		config.SecretsWhitelist = globalConfig.SecretsWhitelist
-
-		if globalConfig.Integrations.Jira != nil {
-			config.Integrations.Jira = globalConfig.Integrations.Jira
+		if extraConfig.SeverityThreshold != "" && validator.ValidateSeverityThreshold(extraConfig) {
+			config.SeverityThreshold = extraConfig.SeverityThreshold
 		}
 
-		if len(globalConfig.Notifications) > 0 && config.Notifications == nil {
-			config.Notifications = globalConfig.Notifications
+		if extraConfig.Integrations.Jira != nil {
+			if config.Integrations.Jira == nil {
+				config.Integrations.Jira = extraConfig.Integrations.Jira
+			} else {
+				if extraConfig.Integrations.Jira.Priorities != nil {
+					config.Integrations.Jira.Priorities = extraConfig.Integrations.Jira.Priorities
+				}
+
+				if extraConfig.Integrations.Jira.Assignee != nil {
+					config.Integrations.Jira.Assignee = extraConfig.Integrations.Jira.Assignee
+				}
+			}
 		}
 
-		for k, v := range globalConfig.Notifications {
+		if len(extraConfig.IgnoreDirs) > 0 {
+			config.IgnoreDirs = extraConfig.IgnoreDirs
+		}
+
+		if len(extraConfig.IgnorePaths) > 0 {
+			config.IgnorePaths = extraConfig.IgnorePaths
+		}
+
+		if extraConfig.Code.AutoFix != nil {
+			config.Code.AutoFix = extraConfig.Code.AutoFix
+		}
+
+		if len(extraConfig.Code.Ignore) > 0 {
+			config.Code.Ignore = extraConfig.Code.Ignore
+		}
+
+		if extraConfig.Dependencies.AutoFix != nil {
+			config.Dependencies.AutoFix = extraConfig.Dependencies.AutoFix
+		}
+
+		if len(extraConfig.Dependencies.Ignore) > 0 {
+			config.Dependencies.Ignore = extraConfig.Dependencies.Ignore
+		}
+
+		if len(extraConfig.Secrets.Ignore) > 0 {
+			config.Secrets.Ignore = extraConfig.Secrets.Ignore
+		}
+
+		if len(extraConfig.SecretsWhitelist) > 0 {
+			config.SecretsWhitelist = extraConfig.SecretsWhitelist
+		}
+
+		config.Secrets.CustomPatternsOverrideGlobal = extraConfig.Secrets.CustomPatternsOverrideGlobal
+
+		if extraConfig.Secrets.CustomPatternsOverrideGlobal {
+			// override global custom patterns with repo custom patterns
+			config.Secrets.CustomPatterns = extraConfig.Secrets.CustomPatterns
+		} else if config.Secrets.CustomPatterns == nil {
+			config.Secrets.CustomPatterns = extraConfig.Secrets.CustomPatterns
+		} else {
+			// merge repo custom patterns with global custom patterns
+			for k, v := range extraConfig.Secrets.CustomPatterns {
+				config.Secrets.CustomPatterns[k] = v
+			}
+		}
+
+		if len(extraConfig.Notifications) > 0 && config.Notifications == nil {
+			config.Notifications = extraConfig.Notifications
+		}
+
+		for k, v := range extraConfig.Notifications {
 			config.Notifications[k] = v
 		}
 
-		if len(globalConfig.ScheduledNotifications) > 0 && config.ScheduledNotifications == nil {
-			config.ScheduledNotifications = globalConfig.ScheduledNotifications
+		if len(extraConfig.ScheduledNotifications) > 0 && config.ScheduledNotifications == nil {
+			config.ScheduledNotifications = extraConfig.ScheduledNotifications
 		}
 
-		for k, v := range globalConfig.ScheduledNotifications {
+		for k, v := range extraConfig.ScheduledNotifications {
 			config.ScheduledNotifications[k] = v
 		}
 	}
 
-	// override with repo config
-
-	if repoConfig == nil {
-		return config
-	}
-
-	if repoConfig.FailBuilds != nil {
-		config.FailBuilds = repoConfig.FailBuilds
-	}
-
-	if repoConfig.SeverityThreshold != "" && validator.ValidateSeverityThreshold(config) {
-		config.SeverityThreshold = repoConfig.SeverityThreshold
-	}
-
-	if repoConfig.Integrations.Jira != nil {
-		config.Integrations.Jira = repoConfig.Integrations.Jira
-		if repoConfig.Integrations.Jira.Priorities != nil {
-			config.Integrations.Jira.Priorities = repoConfig.Integrations.Jira.Priorities
-		} else if globalConfig != nil && globalConfig.Integrations.Jira != nil && globalConfig.Integrations.Jira.Priorities != nil {
-			// copying over repoConfig if globalConfig is set may overwrite the priorities,
-			// so we need to copy over the globalConfig priorities again if repo level priorities are not set
-			config.Integrations.Jira.Priorities = globalConfig.Integrations.Jira.Priorities
-		}
-
-		if repoConfig.Integrations.Jira.Assignee != nil {
-			config.Integrations.Jira.Assignee = repoConfig.Integrations.Jira.Assignee
-		} else if globalConfig != nil && globalConfig.Integrations.Jira != nil && globalConfig.Integrations.Jira.Assignee != nil {
-			// assignee is set in the global config, but not in the repo config
-			config.Integrations.Jira.Assignee = globalConfig.Integrations.Jira.Assignee
-		}
-	}
-
-	if len(repoConfig.IgnoreDirs) > 0 {
-		config.IgnoreDirs = repoConfig.IgnoreDirs
-	}
-
-	if len(repoConfig.IgnorePaths) > 0 {
-		config.IgnorePaths = repoConfig.IgnorePaths
-	}
-
-	if repoConfig.Code.AutoFix != nil {
-		config.Code.AutoFix = repoConfig.Code.AutoFix
-	}
-
-	if len(repoConfig.Code.Ignore) > 0 {
-		config.Code.Ignore = repoConfig.Code.Ignore
-	}
-
-	if repoConfig.Dependencies.AutoFix != nil {
-		config.Dependencies.AutoFix = repoConfig.Dependencies.AutoFix
-	}
-
-	if len(repoConfig.Dependencies.Ignore) > 0 {
-		config.Dependencies.Ignore = repoConfig.Dependencies.Ignore
-	}
-
-	if len(repoConfig.Secrets.Ignore) > 0 {
-		config.Secrets.Ignore = repoConfig.Secrets.Ignore
-	}
-
-	if len(repoConfig.SecretsWhitelist) > 0 {
-		config.SecretsWhitelist = repoConfig.SecretsWhitelist
-	}
-
-	config.Secrets.CustomPatternsOverrideGlobal = repoConfig.Secrets.CustomPatternsOverrideGlobal
-
-	if repoConfig.Secrets.CustomPatternsOverrideGlobal {
-		// override global custom patterns with repo custom patterns
-		config.Secrets.CustomPatterns = repoConfig.Secrets.CustomPatterns
-	} else if config.Secrets.CustomPatterns == nil {
-		config.Secrets.CustomPatterns = repoConfig.Secrets.CustomPatterns
-	} else {
-		// merge repo custom patterns with global custom patterns
-		for k, v := range repoConfig.Secrets.CustomPatterns {
-			config.Secrets.CustomPatterns[k] = v
-		}
-	}
-
-	if len(repoConfig.Notifications) > 0 && config.Notifications == nil {
-		config.Notifications = repoConfig.Notifications
-	}
-
-	for k, v := range repoConfig.Notifications {
-		config.Notifications[k] = v
-	}
-
-	if len(repoConfig.ScheduledNotifications) > 0 && config.ScheduledNotifications == nil {
-		config.ScheduledNotifications = repoConfig.ScheduledNotifications
-	}
-
-	for k, v := range repoConfig.ScheduledNotifications {
-		config.ScheduledNotifications[k] = v
-	}
-
-	return config
+	return &config
 }
