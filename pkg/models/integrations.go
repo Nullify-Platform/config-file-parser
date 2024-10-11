5e82c32d76fbed26ab2cf31249eeec0d36bf469d
Jonathan Lam <jonathanlam@users.noreply.github.com>
2024-10-12 04:25:37 +1100

    add priority threshold to jira (#141)

--- pkg/models/integrations.go
+++ pkg/models/integrations.go
@@ -9,6 +9,7 @@ type Jira struct {
 	ProjectKey        string      `yaml:"project_key,omitempty"`
 	IssueType         string      `yaml:"issue_type,omitempty"`
 	SeverityThreshold string      `yaml:"severity_threshold,omitempty"`
+	PriorityThreshold string      `yaml:"priority_threshold,omitempty"`
 	OnFixTransition   string      `yaml:"on_fix_transition,omitempty"`
 	Priorities        *Priorities `yaml:"priorities,omitempty"`
 	Assignee          *Assignee   `yaml:"assignee,omitempty"`
@@ -17,10 +18,13 @@ type Jira struct {
 // Mapping of Nullify Finding severities to Jira Priorities.
 // The user can specify the priority of the issue based on the severity.
 type Priorities struct {
-	Critical string `yaml:"critical,omitempty"`
-	High     string `yaml:"high,omitempty"`
-	Medium   string `yaml:"medium,omitempty"`
-	Low      string `yaml:"low,omitempty"`
+	Critical   string `yaml:"critical,omitempty"`
+	High       string `yaml:"high,omitempty"`
+	Medium     string `yaml:"medium,omitempty"`
+	Low        string `yaml:"low,omitempty"`
+	Urgent     string `yaml:"urgent,omitempty"`
+	Important  string `yaml:"important,omitempty"`
+	Negligible string `yaml:"negligible,omitempty"`
 }
 
 type Assignee struct {
