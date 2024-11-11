9bdcf2294110a72c2896c014cb2c952b96d1bd4f
JT <justin.ting@nullify.cloud>
2024-11-11 13:34:20 +1100

    feat(AGI-162): compare severities quantitatively (#145)

--- pkg/models/severity.go
+++ pkg/models/severity.go
@@ -7,3 +7,32 @@ const (
 	SeverityLow      = "LOW"
 	SeverityUnknown  = "UNKNOWN"
 )
+
+var severityToInt = map[string]int{
+	SeverityUnknown:  0,
+	SeverityLow:      1,
+	SeverityMedium:   2,
+	SeverityHigh:     3,
+	SeverityCritical: 4,
+}
+
+func CompareSeverity(severity1, severity2 string) int {
+	val1, ok1 := severityToInt[severity1]
+	val2, ok2 := severityToInt[severity2]
+
+	if !ok1 && !ok2 {
+		return 0
+	} else if !ok1 {
+		return -1
+	} else if !ok2 {
+		return 1
+	}
+
+	if val1 < val2 {
+		return -1
+	} else if val1 > val2 {
+		return 1
+	} else {
+		return 0
+	}
+}
