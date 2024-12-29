252c7ecb0ff41f6b92e380995bd482cccf78a805
Jonathan Lam <jonathanlam@users.noreply.github.com>
2024-12-30 02:03:16 +1100

    IsConfigValid utility function (#151)

--- pkg/validator/validate.go
+++ pkg/validator/validate.go
@@ -1,7 +1,11 @@
 package validator
 
 import (
+	"context"
+
 	"github.com/nullify-platform/config-file-parser/pkg/models"
+	"github.com/nullify-platform/config-file-parser/pkg/parser"
+	"github.com/pkg/errors"
 )
 
 // ValidateConfig return true if provided configuration is valid
@@ -11,4 +15,14 @@ func ValidateConfig(config *models.Configuration) bool {
 		ValidateScheduledNotifications(config) &&
 		ValidatePaths(config) &&
 		ValidateAutoFix(config)
+}
+
+func IsConfigValid(ctx context.Context, configString string) (bool, error) {
+	parsedConfig, err := parser.ParseConfiguration([]byte(configString))
+	if err != nil {
+		return false, errors.Wrap(err, "failed to parse config")
+	}
+
+	isValid := ValidateConfig(parsedConfig)
+	return isValid, nil
 }
