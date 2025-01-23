a9bd17c01e8a8a1683f7a2f8dd56a82969a57ba7
Tim Thacker <tim.thacker@nullify.cloud>
2025-01-23 16:35:19 +1100

    add flag to enable attack surface monitoring

--- pkg/models/attack_surface.go
+++ pkg/models/attack_surface.go
@@ -2,6 +2,7 @@ package models
 
 type AttackSurface struct {
 	// global only
+	Enable             bool     `yaml:"enable,omitempty"`
 	EnableDNSTraversal bool     `yaml:"enable_dns_traversal,omitempty"`
 	DomainNames        []string `yaml:"domain_names,omitempty"`
 	IgnoreDomainNames  []string `yaml:"ignore_domain_names,omitempty"`
