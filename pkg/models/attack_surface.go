e3d4935118e8f03d5c682260edbde6d75d66313b
Chris Yoo <chris.yoo@nullify.cloud>
2025-02-11 10:23:45 +1100

    more flexible include_only and ignore rules

--- pkg/models/attack_surface.go
+++ pkg/models/attack_surface.go
@@ -2,12 +2,33 @@ package models
 
 type AttackSurface struct {
 	// global only
-	Enable               bool     `yaml:"enable"`
-	EnableDNSEnumeration bool     `yaml:"enable_dns_enumeration"`
-	DomainNames          []string `yaml:"domain_names,omitempty"`
-	IgnoreDomainNames    []string `yaml:"ignore_domain_names,omitempty"`
-	PathPrefixes         []string `yaml:"path_prefixes,omitempty"`
-	IgnoreMethods        []string `yaml:"ignore_methods,omitempty"`
-	IgnorePorts          []int    `yaml:"ignore_ports,omitempty"`
-	Schemes              []string `yaml:"schemes"`
+	Enable               bool                       `yaml:"enable"`
+	EnableDNSEnumeration bool                       `yaml:"enable_dns_enumeration"`
+	IPAddresses          []string                   `yaml:"ip_addresses,omitempty"`
+	DomainNames          []string                   `yaml:"domain_names,omitempty"`
+	IncludeOnly          []AttackSurfaceIncludeOnly `yaml:"include_only,omitempty"`
+	Ignore               []AttackSurfaceIgnore      `yaml:"ignore,omitempty"`
+}
+
+type AttackSurfaceIncludeOnly struct {
+	DomainNames []string                      `yaml:"domain_names,omitempty"`
+	HTTP        *HTTPAttackSurfaceIncludeOnly `yaml:"http,omitempty"`
+}
+
+type HTTPAttackSurfaceIncludeOnly struct {
+	Paths []string `yaml:"paths,omitempty"`
+}
+
+type AttackSurfaceIgnore struct {
+	// empty fields are equivalent to *
+	IPAddresses        []string                 `yaml:"ip_addresses,omitempty"`
+	DomainNames        []string                 `yaml:"domain_names,omitempty"`
+	TransportProtocols []string                 `yaml:"transport_protocols,omitempty"`
+	Ports              []string                 `yaml:"ports,omitempty"`
+	HTTP               *HTTPAttackSurfaceIgnore `yaml:"http,omitempty"`
+}
+
+type HTTPAttackSurfaceIgnore struct {
+	Methods []string `yaml:"methods,omitempty"`
+	Paths   []string `yaml:"paths,omitempty"`
 }
