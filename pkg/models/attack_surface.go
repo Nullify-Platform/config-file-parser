b8f8a8e659ea4d54ad17f290586a2b8c0b4c74da
Vikranth Subramanian <vikranthsubramanian@192-168-1-117.tpgi.com.au>
2025-02-12 19:10:16 +1100

    update the examples and integration tests

--- pkg/models/attack_surface.go
+++ pkg/models/attack_surface.go
@@ -4,7 +4,7 @@ type AttackSurface struct {
 	// global only
 	Enable               bool                       `yaml:"enable"`
 	EnableDNSEnumeration bool                       `yaml:"enable_dns_enumeration"`
-	AWSIntegration       *AWSIntegration            `yaml:"aws_integration,omitempty"`
+	AWSIntegration       *AWSIntegration            `yaml:"aws_integration"`
 	IPAddresses          []string                   `yaml:"ip_addresses,omitempty"`
 	DomainNames          []string                   `yaml:"domain_names,omitempty"`
 	IncludeOnly          []AttackSurfaceIncludeOnly `yaml:"include_only,omitempty"`
@@ -12,11 +12,11 @@ type AttackSurface struct {
 }
 
 type AWSIntegration struct {
-	EnableAWSIntegration bool      `yaml:"enable_aws_integration"`
-	PrimaryAccountID     string    `yaml:"primary_account_id"`
-	PrimaryRegion        string    `yaml:"primary_region"`
-	TargetRegions        *[]string `yaml:"target_regions,omitempty"`
-	TargetAccounts       *[]string `yaml:"target_accounts,omitempty"`
+	Enable           bool      `yaml:"enable"`
+	PrimaryAccountID string    `yaml:"primary_account_id,omitempty"`
+	PrimaryRegion    string    `yaml:"primary_region,omitempty"`
+	TargetRegions    *[]string `yaml:"target_regions,omitempty"`
+	TargetAccounts   *[]string `yaml:"target_accounts,omitempty"`
 }
 
 type AttackSurfaceIncludeOnly struct {
--- tests/integration_test.go
+++ tests/integration_test.go
@@ -189,11 +189,11 @@ func TestIntegration(t *testing.T) {
 				},
 			},
 			AWSIntegration: &models.AWSIntegration{
-				EnableAWSIntegration: true,
-				PrimaryAccountID:     "123456789012",
-				PrimaryRegion:        "ap-southeast-2",
-				TargetRegions:        &[]string{"ap-southeast-2", "us-east-2"},
-				TargetAccounts:       &[]string{"123456789012", "123456789013"},
+				Enable:           true,
+				PrimaryAccountID: "123456789012",
+				PrimaryRegion:    "ap-southeast-2",
+				TargetRegions:    &[]string{"ap-southeast-2", "us-east-2"},
+				TargetAccounts:   &[]string{"123456789012", "123456789013"},
 			},
 		},
 	}
