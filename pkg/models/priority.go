1206073b377d0bf678adb281facc58ddb81be3d1
JT <justin.ting@nullify.cloud>
2025-04-18 10:05:09 +1000

    chore - add missing unknown priority (#173)

--- pkg/models/priority.go
+++ pkg/models/priority.go
@@ -4,17 +4,28 @@ const (
 	PriorityUrgent        string = "URGENT"
 	PriorityImportant     string = "IMPORTANT"
 	PriorityInformational string = "INFORMATIONAL"
+	PriorityUnknown       string = "UNKNOWN"
 )
 
 var priorityToInt = map[string]int{
-	PriorityInformational: 0,
-	PriorityImportant:     1,
-	PriorityUrgent:        2,
+	PriorityUnknown:       0,
+	PriorityInformational: 1,
+	PriorityImportant:     2,
+	PriorityUrgent:        3,
 }
 
 func ComparePriority(priority1, priority2 string) int {
 	val1, ok1 := priorityToInt[priority1]
 	val2, ok2 := priorityToInt[priority2]
+
+	// If priority1 is NONE and priority2 is not in the map, treat them as equal
+	if priority1 == PriorityUnknown && !ok2 {
+		return 0
+	}
+	// If priority2 is NONE and priority1 is not in the map, treat them as equal
+	if priority2 == PriorityUnknown && !ok1 {
+		return 0
+	}
 
 	if !ok1 && !ok2 {
 		return 0
