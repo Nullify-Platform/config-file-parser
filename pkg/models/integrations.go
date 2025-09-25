787c21e3e2b8ac022e2b94bc98ab54bd659e5af8
Jonathan Lam <jonathanlam@users.noreply.github.com>
2025-09-24 18:45:46 -0700

    new jira integration config (#179)

--- pkg/models/integrations.go
+++ pkg/models/integrations.go
@@ -5,14 +5,19 @@ type Integrations struct {
 }
 
 type Jira struct {
-	Disabled          bool        `yaml:"disabled,omitempty"`
-	ProjectKey        string      `yaml:"project_key,omitempty"`
-	IssueType         string      `yaml:"issue_type,omitempty"`
-	SeverityThreshold string      `yaml:"severity_threshold,omitempty"`
-	PriorityThreshold string      `yaml:"priority_threshold,omitempty"`
-	OnFixTransition   string      `yaml:"on_fix_transition,omitempty"`
-	Priorities        *Priorities `yaml:"priorities,omitempty"`
-	Assignee          *Assignee   `yaml:"assignee,omitempty"`
+	Disabled            bool        `yaml:"disabled,omitempty"`
+	Enabled             *bool       `yaml:"enabled,omitempty"`
+	ProjectKey          string      `yaml:"project_key,omitempty"`
+	IssueType           string      `yaml:"issue_type,omitempty"`
+	SeverityThreshold   string      `yaml:"severity_threshold,omitempty"`
+	PriorityThreshold   string      `yaml:"priority_threshold,omitempty"`
+	OnFixTransition     string      `yaml:"on_fix_transition,omitempty"`
+	CommentOnClose      *bool       `yaml:"comment_on_close,omitempty"`
+	Labels              []string    `yaml:"labels,omitempty"`
+	TitleTemplate       string      `yaml:"title_template,omitempty"`
+	DescriptionTemplate string      `yaml:"description_template,omitempty"`
+	Priorities          *Priorities `yaml:"priorities,omitempty"`
+	Assignee            *Assignee   `yaml:"assignee,omitempty"`
 }
 
 // Mapping of Nullify Finding severities to Jira Priorities.
